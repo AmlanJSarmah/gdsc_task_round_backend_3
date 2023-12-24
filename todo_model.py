49ad8fc564baf0cc452b73720f56e830f6863660
A.J.Sarmah <ajsarmah@tutanota.com>
2023-12-24 15:00:51 +0530

    Delete todo_model.py

--- todo_model.py
+++
@@ -1,6 +0,0 @@
-class Todo:
-    def __init__(self, item_id, title, description, is_completed):
-        self.item_id = item_id
-        self.title = title
-        self.description = description
-        self.is_completed = is_completed
