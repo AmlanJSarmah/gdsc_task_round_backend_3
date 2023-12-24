ff61470f43e61f9ca6dbeb29bfcf8e05fb2292a9
A.J.Sarmah <ajsarmah@tutanota.com>
2023-12-24 15:47:12 +0530

    Created an update method

--- app.py
+++ app.py
@@ -26,11 +26,29 @@ def todo_item(item_id):
 @app.route('/todo/', methods=['POST'])
 def add_todo_item():
     data = request.get_json()
+    data_keys = list(data.keys())
+    for key in data_keys:
+        if (key == 'title' or key == 'description' or key == 'is_completed') is not True:
+            return jsonify({'status': 400, 'message': 'Invalid Data'})
     if not bool(data) is False:
         tl.insert_todo_item(data)
         return jsonify({'data': tl.get_todo_list(), 'status': 200})
     else:
         return jsonify({'data': tl.get_todo_list(), 'status': 200})
+
+
+@app.route('/todo/<int:item_id>', methods=['PUT'])
+def update_item(item_id):
+    data = request.get_json()
+    data_keys = list(data.keys())
+    for key in data_keys:
+        if (key == 'title' or key == 'description' or key == 'is_completed') is not True:
+            return jsonify({'status': 400, 'message': 'Invalid Data'})
+    update_status = tl.update_todo_item(item_id, data)
+    if update_status == 0:
+        return jsonify({'data': tl.get_todo_item(item_id), 'status': 200})
+    else:
+        return jsonify({'status': 404, 'message': 'Item Not Found'})
 
 
 if __name__ == '__main__':
--- todo_list_operations.py
+++ todo_list_operations.py
@@ -28,3 +28,18 @@ def insert_todo_item(item):
     file = open('todo_list.json', 'w')
     json.dump(data, file)
     file.close()
+
+
+def update_todo_item(item_id, new_data):
+    file = open('todo_list.json', 'r')
+    data = json.load(file)
+    file.close()
+    for i in range(len(data)):
+        if int(data[i]['item_id']) == int(item_id):
+            data[i] = new_data
+            data[i]['item_id'] = str(item_id)
+            file = open('todo_list.json', 'w')
+            json.dump(data, file)
+            file.close()
+            return 0
+    return 1
